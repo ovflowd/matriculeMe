620415aedd4896c1ae1b7fdb5682d25eabd194a6
dmcsanches <dmcsanches@gmail.com>
2016-10-26 08:19:38 -0200

    Minor exception handle

--- Artefatos/HistBot.java
+++ Artefatos/HistBot.java
@@ -8,8 +8,11 @@ public class HistBot {
 	WebDriver driver = new SelendroidDriver(capa);
 	driver.get("https://wwwsec.serverweb.unb.br/graduacao/sec/login.aspx");
 	WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
-	wait.Until(driver => !driver.FindElement(By.Name("inputMatricula")));
+	//Espera ate 30 secs para elemento aparecer na tela
+	wait.Until(ExpectedConditions.ElementExists(By.Name("inputMatricula")));
 	//Espera o campo de entrada de matricula sumir
+	wait.Until(ExpectedConditions.not(ExpectedConditions.presenceOfAllElementsLocatedBy(By.Name("inputMatricula")))).catch(Exception);
+	
 	
 	//abre historico
 	driver.get("https://wwwsec.serverweb.unb.br/graduacao/sec/he.aspx");
