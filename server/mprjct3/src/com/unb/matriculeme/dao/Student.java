d34dc62331aabb0160b4f169ae53374d45e12265
conilas <nick.meinen@live.com>
2016-11-21 17:19:01 -0200

    Outra mudanca no student pra fazer teste

--- server/mprjct3/src/com/unb/matriculeme/dao/Student.java
+++ server/mprjct3/src/com/unb/matriculeme/dao/Student.java
@@ -46,6 +46,10 @@ public class Student {
 	@JoinColumn 
 	private Login login;
 
+	@OneToMany(cascade = { CascadeType.MERGE, CascadeType.REFRESH, CascadeType.REMOVE, CascadeType.PERSIST }, fetch = FetchType.EAGER)
+	@JoinTable(name = "aluno_sugestoes")
+	private List<Suggestion> suggestions; 
+
     public int getId() {
         return id;
     }
@@ -115,6 +119,14 @@ public class Student {
     
     public void setLogin(Login login) {
         this.login = login;
+    }
+    
+    public List<Suggestion> getSuggestions(){
+    	return this.suggestions;
+    }
+    
+    public void setSuggestions(List<Suggestion> suggestion){
+    	this.suggestions = suggestion;
     }
     
     public List<CoursedDisciplines> getCoursedDisciplines(){
