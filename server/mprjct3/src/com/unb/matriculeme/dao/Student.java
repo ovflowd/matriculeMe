1e4f379bbfa132887f58961201d76e60ecb7ba93
conilas <nick.meinen@live.com>
2016-11-21 14:36:01 -0200

    Alteracoes no pojo student
    Faltava algumas coisas pra poder funcionar

--- server/mprjct3/src/com/unb/matriculeme/dao/Student.java
+++ server/mprjct3/src/com/unb/matriculeme/dao/Student.java
@@ -1,38 +1,57 @@
 package com.unb.matriculeme.dao;
 
+import java.util.List;
+
 import javax.persistence.*;
+
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+
 
 @Entity
 @Table(name = "aluno")
 public class Student {
 
-    @Id
-    @GeneratedValue
-    private int id;
+	@Id
+	@GeneratedValue
+	private int id; // Chave primária da classe Alunos
 
-    // Matricula from student
-    @Column
-    private int registerId;
+	@Column(nullable = false, unique = true)
+	private int registerId; // Matricula do aluno na Universidade
 
-    @Column
-    private String name;
+	@Column(nullable = false)
+	private String name; // Nome do Aluno
 
-    @Column
-    private int ira;
+	@Column
+	private int ira; // Indice de Rendimento Acadmico do aluno
 
-    @Column(name = "semestre_atual")
-    private int actualSemester;
+	@Column
+	private int actualSemester;
 
-    @OneToOne(cascade = CascadeType.ALL)
-    @JoinTable(name = "curso_aluno")
-    private Course course;
+	@OneToOne(cascade = { CascadeType.MERGE, CascadeType.REFRESH, CascadeType.ALL })
+	@JoinColumn
+	private Course course;// 'FK'
 
-    @Column
-    private String interest;
+	@Column
+	private String interest;
 
-    @OneToOne
-    @JoinTable(name = "login_aluno")
-    private Login login;
+	@OneToOne(cascade = { CascadeType.MERGE, CascadeType.REFRESH, CascadeType.REMOVE, CascadeType.ALL })
+	@JoinColumn
+	@Fetch(FetchMode.SELECT)
+	private Profile profile;
+
+	@OneToMany
+	@JoinTable(name = "disciplinasCursadas") // dar uma olhada no
+	private List<CoursedDisciplines> coursedisciplines;  
+
+	@OneToOne(cascade = { CascadeType.MERGE, CascadeType.REFRESH, CascadeType.REMOVE })
+	@JoinColumn 
+	@Fetch(FetchMode.SELECT)   
+	private Login login;
+
+	@OneToMany(cascade = { CascadeType.MERGE, CascadeType.REFRESH, CascadeType.REMOVE, CascadeType.PERSIST })
+	@JoinTable(name = "aluno_sugestoes") // dar uma olhadad em sugestoes
+	private List<Suggestion> suggestions; 
 
     public int getId() {
         return id;
@@ -93,8 +112,31 @@ public class Student {
     public Login getLogin() {
         return login;
     }
-
+    public void setProfile(Profile profile){
+    	this.profile = profile;
+    }
+    
+    public Profile getProfile(){
+    	return this.profile;
+    }
+    
     public void setLogin(Login login) {
         this.login = login;
+    }
+    
+    public List<Suggestion> getSuggestions(){
+    	return this.suggestions;
+    }
+    
+    public void setSuggestions(List<Suggestion> suggestion){
+    	this.suggestions = suggestion;
+    }
+    
+    public List<CoursedDisciplines> getCoursedDisciplines(){
+    	return this.coursedisciplines;
+    }
+    
+    public void setCoursedDisciplines (List<CoursedDisciplines> coursedDisciplines){
+    	this.coursedisciplines = coursedDisciplines;
     }
 }
