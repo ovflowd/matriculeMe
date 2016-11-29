ec65ba3a21c584ab7d2dcc1c7029b657bb397b5b
BrunoEmerich <bruno.emerich107@gmail.com>
2016-11-29 17:34:36 -0200

    Create Trigger.java

---
+++ server/mprjct3/src/com/unb/matriculeme/domain/Trigger.java
@@ -0,0 +1,20 @@
+package com.unb.matriculeme.domain;
+
+import com.unb.matriculeme.helpers.RestClient;
+
+public class Trigger extends Thread
+{
+	private String data;
+	private String path;
+	
+	public Trigger(String data,String path)
+	{
+		this.data = data;
+		this.path = path;
+	}
+	@Override
+	public void run() 
+	{
+		RestClient.sendData(this.data,this.path);
+	}
+}
